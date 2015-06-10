4a4b5d6bbe9985c70424783c0aa695105da4a10e
Jay Park <jay.park@bluefletch.com>
2015-06-09 21:13:48 -0400

    starting suits

---
+++ HKTicketsSwift/HKTicketsSwift/SuitOrderInfo.swift
@@ -0,0 +1,15 @@
+//
+//  SuitOrderInfo.swift
+//  HKTicketsSwift
+//
+//  Created by Jay Park on 6/9/15.
+//  Copyright © 2015 JPApps. All rights reserved.
+//
+
+import UIKit
+
+class SuitOrderInfo: NSObject {
+    var jacketOrderInfo = JacketOrderInfo();
+    var pantsOrderInfo = PantsOrderInfo();
+//    var vestOrderInfo : VestOrderInfo?;
+}
