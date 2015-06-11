3a63c445e73b0309425544a17cbce90dd4bfe158
Jay Park <jay.park@bluefletch.com>
2015-06-11 14:31:02 -0400

    continuing jacket work

---
+++ HKTicketsSwift/HKTicketsSwift/SuitFabricTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SuitFabricTableViewCell.swift
+//  HKTicketsSwift
+//
+//  Created by Jay Park on 6/10/15.
+//  Copyright © 2015 JPApps. All rights reserved.
+//
+
+import UIKit
+
+class SuitFabricTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
